a9084c55604ece3b252073fc9b216c6f11b1f5f5
Oscarj-jqt <o_jacquet2@hetic.eu>
2024-06-04 13:33:55 +0200

    first commit

---
+++ student_bdd/src/studentdatabaseapp/Student.java
@@ -0,0 +1,71 @@
+package studentdatabaseapp;
+
+import java.util.Scanner;
+
+public class Student {
+    // On créer les propriétés
+    private String firstName;
+    private String lastName;
+    private int gradeYear;
+    private String studentID;
+    private String courses = null;
+    private int tuitionBalance;
+    private static int costOfCourse = 600;
+    private static int id = 1000;
+
+    //On établit maintenant les méthodes, les constructeur pour les fonctionnalités de notre app
+    //Constructeur prompt au user de mettre son nom et son année
+    public Student() {
+//        Un input
+        Scanner in = new Scanner(System.in);
+        System.out.print("Entrez le prénom de l'étudiant : ");
+        this.firstName = in.nextLine();
+
+        System.out.print("Entrez le nom de l'étudiant : ");
+        this.lastName = in.nextLine();
+
+        System.out.print("1 - Première année \n2 - Deuxième année " +
+                "\n3- Junior \n4- Sénior\n Entrez le grade de l'étudiant : ");
+        this.gradeYear = in.nextInt();
+
+        //lorsque l'on appelle notre méthode setStudentID, l'id incrémente de 1
+        setStudentID();
+
+
+        System.out.println(firstName + " " + lastName + " " + gradeYear + "" + studentID);
+
+
+    }
+
+    //Méthode, setter pour générer son ID
+//    Il doit avoir leur propre ID --> avec static, elle ne renvoie rien donc void
+    private void setStudentID() {
+        //Son grade + son ID
+        id++;
+        this.studentID = gradeYear + " " + id;
+    }
+
+    //Inscription aux cours
+    public void enroll() {
+        // On fait une boucle
+        do {
+            System.out.println("Entrez le cours (Q pour quitter): ");
+            Scanner in = new Scanner(System.in);
+            String course = in.nextLine();
+            if (!course.equals("Q")) {
+                courses = courses + "\n" + course;
+                tuitionBalance = tuitionBalance + costOfCourse;
+            }
+            else {
+                System.out.println("Break !");
+                break;
+            }
+        } while (1 != 0);
+        System.out.println("Inscrit à :" + courses);
+        System.out.println("Frais de scolarité :" + tuitionBalance);
+    }
+
+    // Voir le solde
+
+    // Payer les frais de scolarité
+}
---
+++ student_bdd/src/studentdatabaseapp/StudentDatabaseApp.java
@@ -0,0 +1,14 @@
+package studentdatabaseapp;
+
+public class StudentDatabaseApp {
+
+    public static void main(String[] args) {
+//        On crée un nouvel étudiant
+        Student stu1 = new Student();
+        stu1.enroll();
+        //Demander le nombre d'étudiants que l'on a
+
+
+        // Créer n nombre de nouveaux étudiants
+    }
+}
